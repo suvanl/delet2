0d6fd66e48151da3a292f265e1f74a468904cdfd
SuvanL <mr.slrmx@gmail.com>
2018-01-23 10:56:05 +0000

    Create lockdown command

---
+++ commands/lockdown.js
@@ -0,0 +1,52 @@
+const Moderation = require('../base/Moderation.js');
+const ms = require('ms');
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+class Lockdown extends Moderation {
+    constructor(client) {
+      super(client, {
+        name: 'lockdown',
+        description: 'Locks a channel down for a set duration. Use "%lockdown release" to end the lockdown prematurely.',
+        usage: 'lockdown <duration> <s|m|h>',
+        guildOnly: true,
+        aliases: ['ld']
+      });
+    }
+
+    async run(message, args, level) {
+        if (!client.lockit) client.lockit = [];
+        let time = args.join(' ');
+        let validUnlocks = ["release", "unlock"];
+        if (!time) return message.channel.send("A duration for the lockdown must be set. This can be in hours, minutes or seconds. Example command usage:\n```%lockdown 5 m```");
+
+        try {
+            if (validUnlocks.includes(time)) {
+                message.channel.overwritePermissions(message.guild.id, {
+                    SEND_MESSAGES: null
+                }).then(() => {
+                    message.channel.send("Lockdown lifted.");
+                    clearTimeout(client.lockit[message.channel.id]);
+                    delete client.lockit[message.channel.id];
+                });
+            } else {
+                message.channel.overwritePermissions(message.guild.id, {
+                    SEND_MESSAGES: false
+                }).then(() => {
+                    message.channel.send(`Channel locked down for ${ms(ms(time), { long: true })}.`).then (() => {
+                        client.lockit[message.channel.id] = setTimeout(() => {
+                            message.channel.overwritePermissions(message.guild.id, {
+                                SEND_MESSAGES: null
+                            }).then(message.channel.send("Lockdown lifted."));
+                            delete client.lockit[message.channel.id];
+                        }, ms(time));
+                    });
+                });
+            }
+        } catch (error) {
+            message.channel.send("An error occurred whilst trying to lock this channel down. Example command usage:\n```%lockdown 5 m```")
+        }
+    }
+}
+
+module.exports = Lockdown;
