5d48ffdcbafb24ecb5c571d81a50a99622756abb
SuvanL <mr.slrmx@gmail.com>
2018-01-23 11:26:32 +0000

    Create kick command

---
+++ commands/kick.js
@@ -0,0 +1,43 @@
+const Moderation = require('../base/Moderation.js');
+const Discord = require("discord.js");
+
+class Kick extends Moderation {
+    constructor(client) {
+      super(client, {
+        name: 'kick',
+        description: 'Kicks the mentioned user from the server.',
+        usage: 'kick [user] <reason/info>',
+        guildOnly: true,
+        aliases: ['boot']
+      });
+    }
+    
+    async run(message, args, level) {
+    
+      const user = message.mentions.users.first();
+      const reason = args.slice(1).join(' ');
+      const modLog = message.guild.channels.find("name", "delet-this");
+      if (!modLog) return message.channel.send("Modlog not found. Please inform the server owner of this.");
+      if (!user) return message.channel.send("You must mention a user to kick.");
+      if (!reason) return message.channel.send("Please provide a reason for the punishment.");
+
+      if (!message.guild.member(user).kickable) return message.reply("I cannot kick that user from this server!\nThis may be because I do not have the required permissions to do so, or they may be the server owner.");
+      try {
+        message.guild.member(user).kick();
+        message.react("👌");
+      } catch (error) {
+        return message.channel.send("An error occurred whilst trying to kick the mentioned user.");
+      }
+
+      const embed = new Discord.RichEmbed()
+      .setTitle(`👢 Member kicked in #${message.channel.name}`)
+      .setColor(16733525)
+      .setDescription(`\`\`\`ruby\nIssued to: ${user.tag} (${user.id})\nIssued by: ${message.author.tag} (${message.author.id})\nReason: ${reason}\`\`\``)
+      .setFooter("Moderation system powered by delet™", "https://i.imgur.com/No7WfpC.png")
+      .setTimestamp()
+
+      this.client.channels.get(modLog.id).send({embed});
+    }
+}
+
+module.exports = Kick;
