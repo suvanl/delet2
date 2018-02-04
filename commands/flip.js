75ea212d00461b065f3659784b93bcd65f6c4126
SuvanL <mr.slrmx@gmail.com>
2018-02-04 18:35:50 +0000

    Add currency setting & add Euros. (flip.js)

--- commands/flip.js
+++ commands/flip.js
@@ -13,6 +13,14 @@ class Flip extends Command {
   }
 
   async run(message, args, level) {
+    const settings = message.guild ? this.client.getSettings(message.guild.id) : this.client.settings.get("default");
+
+    // No currency set
+    if (settings.currency === ">>No currency set<<") return message.channel.send(`
+I cannot run this command, as I don't know which currency to use on this server. Please set a currency by using:
+\`\`\`${settings.prefix}set edit currency TYPE\`\`\`
+Currently available types: \`GBP\``);
+
     function coinFlip() {
       return (Math.floor(Math.random() * 2) == 0) ? 'Heads' : 'Tails';
     }
@@ -20,7 +28,10 @@ class Flip extends Command {
     let variable = coinFlip();
     // console.log(variable);
 
-    if(variable === 'Heads') {
+    // Pounds [£] (GBP)
+    if (settings.currency === "GBP") {
+
+    if (variable === 'Heads') {
       const embed = new Discord.RichEmbed()
       .setDescription(`This flip's result was **heads**!\n\nTime taken: ${Math.round(this.client.ping)}ms\nCurrency: Pound Sterling (GBP)`)
       .setThumbnail("https://vgy.me/yvnN22.png")
@@ -31,6 +42,23 @@ class Flip extends Command {
       .setThumbnail("https://vgy.me/PfWWql.png")
       message.channel.send({embed});
     }
+  }
+
+  // Euros [€] (EUR)
+  if (settings.currency === "EUR") {
+    if (variable === 'Heads') {
+      const embed = new Discord.RichEmbed()
+      .setDescription(`This flip's result was **heads**!\n\nTime taken: ${Math.round(this.client.ping)}ms\nCurrency: Euro (EUR)`)
+      .setThumbnail("https://vgy.me/QgVomc.png")
+      message.channel.send({embed});
+    } else {
+      const embed = new Discord.RichEmbed()
+      .setDescription(`This flip's result was **tails**!\n\nTime taken: ${Math.round(this.client.ping)}ms\nCurrency: Euro (EUR)`)
+      .setThumbnail("https://vgy.me/g4ispA.png")
+      message.channel.send({embed});
+    }
+  }
+
   }
 }
 
