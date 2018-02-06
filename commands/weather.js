fbd0700794fd2aed28e5769555b15c11f3572338
SuvanL <mr.slrmx@gmail.com>
2018-02-06 12:51:19 +0000

    Create weather command

---
+++ commands/weather.js
@@ -0,0 +1,44 @@
+const Command = require("../base/Command.js");
+const Discord = require("discord.js");
+const weather = require("weather-js");
+
+class Weather extends Command {
+    constructor(client) {
+      super(client, {
+        name: "weather",
+        description: "Displays weather information for the specified location.",
+        category: "Information",
+        usage: "weather [location]",
+        aliases: [""]
+      });
+    }
+
+    async run(message, args, level) {
+        const settings = message.guild ? this.client.getSettings(message.guild.id) : this.client.settings.get("default");
+
+        weather.find({search: args.join(" "), degreeType: 'C'}, function(err, result) {
+            if (err) return message.channel.send(`An error occurred:\n\`\`\`${err}\`\`\`\nTo see how to use this command, type \`${settings.prefix}help weather\`.`);
+
+            var current = result[0].current;
+            var location = result[0].location;
+
+            const embed = new Discord.RichEmbed()
+            .setColor(7654911)
+            .setTitle(`Current weather information for ${current.observationpoint}`)
+            .setDescription(`The weather is **${current.skytext.toLowerCase()}** at the moment.
+
+• Temperature: **${current.temperature}°C**
+• Feels like: **${current.feelslike}°C**
+• Humidity: **${current.humidity}%**
+• Wind: **${current.winddisplay.toLowerCase()}**
+            `)
+            .setThumbnail(current.imageUrl)
+            .setFooter(`Weather information system powered by delet™`)
+            .setTimestamp()
+
+            message.channel.send({embed});
+        });
+    }
+}
+
+module.exports = Weather;
