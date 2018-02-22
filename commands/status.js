caeb5b3a747a8568d99cf8daa560114abfc98825
Suvan <mr.slrmx@gmail.com>
2018-02-22 19:16:51 +0000

    Create status command

---
+++ commands/status.js
@@ -0,0 +1,26 @@
+const Command = require("../base/Command.js");
+
+class Status extends Command {
+    constructor(client) {
+      super(client, {
+        name: "status",
+        description: "Sets delet's presence/status.",
+        usage: "status",
+        aliases: [""],
+        permLevel: "Bot Admin"
+      });
+    }
+
+    async run(message, args, level) {
+        var status = args.join(" ");
+
+        if (status === "online" || status === "idle" || status === "dnd" || status === "invisible") {
+            this.client.user.setStatus(status);
+            message.channel.send(`Status successfully changed to **${status}**.\nPlease note that changing status may take up to a minute or two.`);
+        } else {
+            return message.channel.send(`"${status}" is not a valid status type.`);
+        }
+    }
+}
+
+module.exports = Status;
