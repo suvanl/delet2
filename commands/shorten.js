06104153c87d8b57a274257bdaee690eb6c6d428
Suvan <mr.slrmx@gmail.com>
2018-02-24 17:57:42 +0000

    Minor text changes

--- commands/shorten.js
+++ commands/shorten.js
@@ -16,12 +16,12 @@ class Shorten extends Command {
       if (!args[1]) {
           shortener.shorten(args[0], function(res) {
               if (res.startsWith("Error:")) return message.channel.send("Invalid URL (link).");
-              message.channel.send(`Your shortened link: **<${res}>**`);
+              message.channel.send(`Your shortened link: **<${res}>**.`);
           });
       } else {
           shortener.custom(args[0], args[1], function(res) {
               if (res.startsWith("Error:")) return message.channel.send(`An error occurred:\n\`\`\`${res}\`\`\``);
-              message.channel.send(`Your shortened link: **<${res}>**`);
+              message.channel.send(`Your shortened link: **<${res}>**.`);
           })
       }
   }
