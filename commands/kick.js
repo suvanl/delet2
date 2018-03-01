6640374698e30b0c59cd296c574ed9f9b7bc7d4a
Suvan <mr.slrmx@gmail.com>
2018-03-01 14:43:26 +0000

    Ensure strings use doublequotes (kick.js)

--- commands/kick.js
+++ commands/kick.js
@@ -1,22 +1,22 @@
-const Command = require('../base/Command.js');
+const Command = require("../base/Command.js");
 const Discord = require("discord.js");
 
 class Kick extends Command {
     constructor(client) {
       super(client, {
-        name: 'kick',
-        description: 'Kicks the mentioned user from the server.',
-        category: 'Moderation',
-        usage: 'kick [user] <reason/info>',
+        name: "kick",
+        description: "Kicks the mentioned user from the server.",
+        category: "Moderation",
+        usage: "kick [user] <reason/info>",
         guildOnly: true,
-        aliases: ['boot'],
+        aliases: ["boot"],
         permLevel: "DeletMod"
       });
     }
     
     async run(message, args, level) { // eslint-disable-line no-unused-vars
       const user = message.mentions.users.first();
-      const reason = args.slice(1).join(' ');
+      const reason = args.slice(1).join(" ");
       const modLog = message.guild.channels.find("name", "delet-this");
       if (!modLog) return message.channel.send("Modlog not found. Please inform the server owner of this.");
       if (!user) return message.channel.send("You must mention a user to kick.");
