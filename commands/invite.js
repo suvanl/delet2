0dcc5683c89de13b3e4f15583091734293eb973d
Suvan <mr.slrmx@gmail.com>
2018-03-03 17:45:44 +0000

    Make stuff cleaner (invite.js)

--- commands/invite.js
+++ commands/invite.js
@@ -11,10 +11,27 @@ class Invite extends Command {
     }
 
     async run(message, args, level) { // eslint-disable-line no-unused-vars
-        this.client.generateInvite(["MANAGE_ROLES", "KICK_MEMBERS", "MANAGE_CHANNELS", "BAN_MEMBERS", "VIEW_AUDIT_LOG", "VIEW_CHANNEL", "SEND_TTS_MESSAGES", "EMBED_LINKS", "READ_MESSAGE_HISTORY", "USE_EXTERNAL_EMOJIS", "SEND_MESSAGES", "MANAGE_MESSAGES", "ATTACH_FILES", "ADD_REACTIONS", "CONNECT", "SPEAK", "USE_VAD"])
-            .then(link => {
-                message.channel.send(`Generated bot invite link: ${link}`);
-            });
+        this.client.generateInvite([
+            "MANAGE_ROLES", 
+            "KICK_MEMBERS", 
+            "MANAGE_CHANNELS", 
+            "BAN_MEMBERS", 
+            "VIEW_AUDIT_LOG", 
+            "VIEW_CHANNEL", 
+            "SEND_TTS_MESSAGES", 
+            "EMBED_LINKS", 
+            "READ_MESSAGE_HISTORY", 
+            "USE_EXTERNAL_EMOJIS", 
+            "SEND_MESSAGES", 
+            "MANAGE_MESSAGES", 
+            "ATTACH_FILES", 
+            "ADD_REACTIONS", 
+            "CONNECT", 
+            "SPEAK", 
+            "USE_VAD"
+        ]).then(link => {
+            message.channel.send(`Generated bot invite link: ${link}`);
+        });
     }
 }
 
