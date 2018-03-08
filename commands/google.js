a80040407f78c283a413c3c3ce5149d85f9ca49f
Suvan <mr.slrmx@gmail.com>
2018-03-08 21:09:01 +0000

    Change `let` to `const` for searchURL (never reassigned)

--- commands/google.js
+++ commands/google.js
@@ -15,7 +15,7 @@ class Google extends Command {
 
     async run(message, args, level) { // eslint-disable-line no-unused-vars
         const searchMessage = await message.channel.send("Searching Google...");
-        let searchURL = `https://www.google.com/search?q=${encodeURIComponent(message.content)}`;
+        const searchURL = `https://www.google.com/search?q=${encodeURIComponent(message.content)}`;
 
         return snekfetch.get(searchURL).then((result) => {
 
