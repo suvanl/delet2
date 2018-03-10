6acfc76a22b7e8a88354477ef165fb6fbfbd95f0
Suvan <mr.slrmx@gmail.com>
2018-03-10 13:56:57 +0000

    Create cat.js

---
+++ commands/Fun/cat.js
@@ -0,0 +1,31 @@
+const Command = require("../../base/Command.js");
+const snekfetch = require("snekfetch");
+
+class Cat extends Command {
+    constructor(client) {
+      super(client, {
+        name: "cat",
+        description: "Sends a random image or GIF of a cat.",
+        category: "Fun",
+        usage: "cat",
+        aliases: [""],
+        botPerms: ["ATTACH_FILES"]
+      });
+    }
+
+    async run(message, args, level) { // eslint-disable-line no-unused-vars
+        const { body } = await snekfetch.get("https://random.cat/meow");
+        try {
+            message.channel.send("Unfortunately, the cat API can sometimes be quite slow. Please wait for your image to be fetched, this is not an error.")
+                .then(msg => {
+                    msg.delete(10000);
+                });
+            message.channel.send({files: [body.file] });
+        } catch (error) {
+            console.log(error);
+            message.channel.send(`An error occurred:\n${error.message}`);
+        }
+    }
+}
+
+module.exports = Cat;
