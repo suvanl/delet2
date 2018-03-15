32152d9e30c982d2e9db88c24249c961ebe85fd5
Suvan <mr.slrmx@gmail.com>
2018-03-15 19:21:34 +0000

    Create shutdown command

---
+++ commands/System/shutdown.js
@@ -0,0 +1,28 @@
+const Command = require("../../base/Command.js");
+
+class Shutdown extends Command {
+  constructor(client) {
+    super(client, {
+      name: "shutdown",
+      description: "Shuts down delet.",
+      category: "System",
+      usage: "shutdown",
+      aliases: ["kill", "endprocess"],
+      permLevel: "Bot Owner"
+    });
+  }
+
+  async run(message, args, level) { // eslint-disable-line no-unused-vars
+    try {
+      await message.channel.send("Shutting down...");
+      this.client.commands.forEach(async cmd => {
+        await this.client.unloadCommand(cmd);
+      });
+      process.exit(0);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
+
+module.exports = Shutdown;
