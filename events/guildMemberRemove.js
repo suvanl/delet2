03c79b948b8375bb26386200e721697b9d93f6e0
Suvan <mr.slrmx@gmail.com>
2018-03-28 19:55:04 +0100

    Create guildMemberRemove event

---
+++ events/guildMemberRemove.js
@@ -0,0 +1,22 @@
+// This event executes when a member leaves a server. Notifies users in the server that a user has left.
+
+module.exports = class {
+    constructor(client) {
+      this.client = client;
+    }
+  
+    async run(member) {
+    // Loads the guild's settings
+      const settings = this.client.getSettings(member.guild.id);
+    
+      // If "welcome" is off, don't proceed
+      if (settings.welcomeEnabled !== "true") return;
+  
+      // Replaces the placeholders in the welcome message with actual data.
+      const leaveMessage = settings.leaveMessage.replace("{{user}}", member.user.tag);
+  
+      // Sends the leave message to the welcome channel.
+      member.guild.channels.find("name", settings.welcomeChannel).send(leaveMessage).catch(console.error);
+    }
+  };
+  
