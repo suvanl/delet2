98be7b3333c56f8142603a019a965fb258af2058
Suvan <mr.slrmx@gmail.com>
2018-03-29 14:42:31 +0100

    Create method to notify guild owner that bot has joined (guildCreate.js)

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -5,13 +5,20 @@ module.exports = class {
     this.client = client;
   }
 
-  async run(guild) {
-
-    // OLD status; no longer used:
-    //this.client.user.setPresence({game: {name: `${this.client.settings.get("default").prefix}help | ${this.client.guilds.size} Servers`, type:0}});
-
-    // Updates number of guilds (servers) on the bot's status.
+  async run(guild, message) {
+    // Updates number of guilds (servers) on the bot's status
     this.client.user.setActivity(`over ${this.client.guilds.size} servers`, {type:"WATCHING"});
+
+    // Logs it
     this.client.logger.log(`New guild has been joined: ${guild.name} (${guild.id}) with ${guild.memberCount - 1} members`);
+
+    // Notify server owner
+    const guildOwner = guild.owner;
+    guildOwner.send(`
+Hey! I'm delet, and I was invited to your server "**${guild.name}**" by someone with the Manage Server permission there.
+To see how to get started, please go to **https://delet.js.org/#docs**.
+
+Hope to be of service to you and your server!
+`);
   }
 };
