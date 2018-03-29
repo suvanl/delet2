2f938602205b37ce843b41778b82f3a4335047bb
Suvan <mr.slrmx@gmail.com>
2018-03-29 14:18:09 +0100

    Create prime.js

---
+++ commands/Maths/prime.js
@@ -0,0 +1,42 @@
+const Command = require("../../base/Command.js");
+
+class Prime extends Command {
+    constructor(client) {
+      super(client, {
+        name: "prime",
+        description: "Checks if a number is a prime number.",
+        category: "Maths",
+        usage: "prime [number]",
+        aliases: ["primenumber", "isprime"]
+      });
+    }
+
+    async run(message, args, level) { // eslint-disable-line no-unused-vars
+        const n = parseInt(args[0]);
+        if (!n) return message.channel.send("You must specify a number to check.");
+        
+        function isPrime(n) {
+            if (isNaN(n) || !isFinite(n) || n%1 || n < 2) return false;
+            if (n%2 == 0) return (n == 2);
+            if (n%3 == 0) return (n == 3);
+            
+            let m = Math.sqrt(n);
+
+            for (let i = 5; i <= m; i += 6) {
+                if (n%i == 0) return false;
+                if (n%(i + 2) == 0) return false;
+            }
+            return true;
+        }
+
+        if (isPrime(n) === true) {
+            message.channel.send(`<:tick:398228298842374154> **${n}** is a prime number.`);
+        }
+
+        if (isPrime(n) === false) {
+            message.channel.send(`<:redX:398228298708025344> **${n}** is not a prime number.`);
+        }
+    }
+}
+
+module.exports = Prime;
