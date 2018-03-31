531522b1112c58ffea6f9f3cae08ff98faadeff5
Suvan <mr.slrmx@gmail.com>
2018-03-31 11:48:57 +0100

    Create dsdev.js

---
+++ commands/Information/dsdev.js
@@ -0,0 +1,19 @@
+const Command = require("../../base/Command.js");
+
+class DSDev extends Command {
+    constructor(client) {
+      super(client, {
+        name: "dsdev",
+        description: "Interested in joining my development team?",
+        category: "Information",
+        usage: "dsdev",
+        aliases: ["hireme", "joinus"]
+      });
+    }
+
+    async run(message, args, level) { // eslint-disable-line no-unused-vars
+        message.channel.send("Interested in joining the team that develops and maintains me?\nVisit this link: **https://delet.js.org/join**.");
+    }
+}
+
+module.exports = DSDev;
