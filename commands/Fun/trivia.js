c3954131586df5bcf44f7bc3a5156e6569883f9b
Suvan <mr.slrmx@gmail.com>
2018-04-03 13:53:47 +0100

    Temporarily disable trivia command

--- commands/Fun/trivia.js
+++ commands/Fun/trivia.js
@@ -10,6 +10,7 @@ class Trivia extends Command {
       category: "Fun",
       usage: "trivia",
       aliases: ["randomtrivia", "randomq", "testme"],
+      permLevel: "Bot Owner",
       guildOnly: true
     });
   }
