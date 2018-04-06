d73d9dfd0d0cb9efc49f53d76fbc08b923072880
Suvan <mr.slrmx@gmail.com>
2018-04-06 11:50:27 +0100

    Create bans.js

---
+++ commands/Moderation/bans.js
@@ -0,0 +1,27 @@
+const Command = require("../../base/Command.js");
+
+class Bans extends Command {
+    constructor(client) {
+      super(client, {
+        name: "bans",
+        description: "Checks how many users are banned on the current server.",
+        category: "Moderation",
+        usage: "bans",
+        guildOnly: true,
+        aliases: ["fetchbans"],
+        permLevel: "Moderator"
+      });
+    }
+
+    async run(message, args, level) { // eslint-disable-line no-unused-vars
+        try {
+            message.guild.fetchBans()
+                .then(bans => message.channel.send(`This server has **${bans.size}** banned user(s).`));
+        } catch (error) {
+            this.client.logger.error(error.message);
+            message.channel.send(`An error occurred:\n\`\`\`${error.message}\`\`\``);
+        }
+    }
+}
+
+module.exports = Bans;
