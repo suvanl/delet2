82f21cca899e91ca4cf936c006489fac6bd7468c
Suvan <mr.slrmx@gmail.com>
2018-04-07 12:04:51 +0100

    Create vaportext command

---
+++ commands/Fun/vaportext.js
@@ -0,0 +1,26 @@
+const Command = require("../../base/Command.js");
+
+class Vaportext extends Command {
+    constructor(client) {
+      super(client, {
+        name: "vaportext",
+        description: "A E S T H E T I C",
+        category: "Fun",
+        usage: "vaportext [text]",
+        aliases: ["vapor"]
+      });
+    }
+
+    async run(message, args, level) { // eslint-disable-line no-unused-vars
+        if (!args.length) return message.channel.send("You must provide some text to make ***A E S T H E T I C A L L Y   P L E A S I N G***.");
+
+        let msg = "";
+        for (let i = 0; i < args.length; i++) {
+            msg += args[i].toUpperCase().split("").join(" ") + "   ";
+        }
+
+        return message.channel.send(msg);
+    }
+}
+
+module.exports = Vaportext;
