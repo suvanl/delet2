62c0bd5fab49358b3b171aa326dc02a58da00af1
Suvan <mr.slrmx@gmail.com>
2018-04-10 10:30:22 +0100

    Create event command

---
+++ commands/System/event.js
@@ -0,0 +1,26 @@
+// The EVENT command will emit an event. This is useful, but should only
+// be used for debugging purposes.
+
+const Command = require("../../base/Command.js");
+
+class Event extends Command {
+    constructor(client) {
+        super(client, {
+            name: "event",
+            description: "Emits an event.",
+            category: "System",
+            usage: "event",
+            aliases: ["emit"],
+            permLevel: "Bot Owner"
+        });
+    }
+
+    async run(message, args, level) { // eslint-disable-line no-unused-vars
+        const member = message.mentions.members.first() || message.member;
+
+        await this.client.emit("guildMemberAdd", member);
+        await this.client.emit("guildMemberRemove", member);
+    }
+}
+
+module.exports = Event;
