ce2591375025907912315e921d2019d52852c4e8
Suvan <mr.slrmx@gmail.com>
2018-04-11 11:48:11 +0100

    Add delet ASCII text to `npm test` script

---
+++ util/text.js
@@ -0,0 +1,21 @@
+console.log(`
+                                                                                          
+            dddddddd                                                                      
+            d::::::d                   lllllll                              tttt          
+            d::::::d                   l:::::l                           ttt:::t          
+            d::::::d                   l:::::l                           t:::::t          
+            d:::::d                    l:::::l                           t:::::t          
+    ddddddddd:::::d     eeeeeeeeeeee    l::::l     eeeeeeeeeeee    ttttttt:::::ttttttt    
+  dd::::::::::::::d   ee::::::::::::ee  l::::l   ee::::::::::::ee  t:::::::::::::::::t    
+ d::::::::::::::::d  e::::::eeeee:::::eel::::l  e::::::eeeee:::::eet:::::::::::::::::t    
+d:::::::ddddd:::::d e::::::e     e:::::el::::l e::::::e     e:::::etttttt:::::::tttttt    
+d::::::d    d:::::d e:::::::eeeee::::::el::::l e:::::::eeeee::::::e      t:::::t          
+d:::::d     d:::::d e:::::::::::::::::e l::::l e:::::::::::::::::e       t:::::t          
+d:::::d     d:::::d e::::::eeeeeeeeeee  l::::l e::::::eeeeeeeeeee        t:::::t          
+d:::::d     d:::::d e:::::::e           l::::l e:::::::e                 t:::::t    tttttt
+d::::::ddddd::::::dde::::::::e         l::::::le::::::::e                t::::::tttt:::::t
+ d:::::::::::::::::d e::::::::eeeeeeee l::::::l e::::::::eeeeeeee        tt::::::::::::::t
+  d:::::::::ddd::::d  ee:::::::::::::e l::::::l  ee:::::::::::::e          tt:::::::::::tt
+   ddddddddd   ddddd    eeeeeeeeeeeeee llllllll    eeeeeeeeeeeeee            ttttttttttt  
+                                                                                                                                                                            
+`);
