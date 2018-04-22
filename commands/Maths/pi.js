e0864fbc921a53d5fe2e19f313d78d80588264f6
Suvan <mr.slrmx@gmail.com>
2018-04-22 18:30:20 +0100

    Create Pi command

---
+++ commands/Maths/pi.js
@@ -0,0 +1,24 @@
+const Command = require("../../base/Command.js");
+
+class Pi extends Command {
+    constructor(client) {
+      super(client, {
+        name: "pi",
+        description: "Returns the value of Pi (π).",
+        usage: "pi",
+        aliases: ["π"]
+      });
+    }
+
+    async run(message, args, level) { // eslint-disable-line no-unused-vars
+        const settings = message.guild ? this.client.getSettings(message.guild.id) : this.client.settings.get("default");
+        if (message.content.startsWith(`${settings.prefix}π`)) {
+            return message.channel.send(`π = **${Math.PI}**...`);
+        }
+        const msg = await message.channel.send(`π = **${Math.PI}**...`);
+        await this.client.wait(500);
+        msg.edit(`π = **${Math.PI}**...\n\nFind out more about Pi here: **<https://en.wikipedia.org/wiki/Pi>**.\nOne million digits of Pi: **<https://www.piday.org/million/>**.`);
+    }
+}
+
+module.exports = Pi;
