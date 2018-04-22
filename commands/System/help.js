3ab70e46d7c653f6d48b457ee9f18f0c98af410c
Suvan <mr.slrmx@gmail.com>
2018-04-22 18:30:31 +0100

    Update help.js

--- commands/System/help.js
+++ commands/System/help.js
@@ -31,7 +31,7 @@ class Help extends Command {
       const commandNames = myCommands.keyArray();
       const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
       let currentCategory = "";
-      let output = "= Command List =\n\n[Use %help <commandname> for details]\n";
+      let output = `= Command List =\n\n[Use ${settings.prefix}help <commandname> for details]\n`;
       const sorted = myCommands.array().sort((p, c) => p.help.category > c.help.category ? 1 :  p.help.name > c.help.name && p.help.category === c.help.category ? 1 : -1 );
       sorted.forEach( c => {
         const cat = c.help.category.toProperCase();
