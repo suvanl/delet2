cc3f874f0c580e02bdb6b6aff599f34a16213060
Suvan <mr.slrmx@gmail.com>
2018-04-23 16:47:52 +0100

    Create heapdump command

---
+++ commands/Debug/heapdump.js
@@ -0,0 +1,28 @@
+const Command = require("../../base/Command.js");
+const { promisify } = require("util");
+const writeSnapshot = promisify(require("heapdump").writeSnapshot);
+
+class HeapDump extends Command {
+  constructor(client) {
+    super(client, {
+      name: "heapdump",
+      description: "Writes a memory snapshot file (useful for debugging memory leaks, etc).",
+      category: "Debug",
+      usage: "heapdump",
+      aliases: ["sysdump", "dump"],
+      permLevel: "Bot Owner"
+    });
+  }
+
+  async run(message, args, level) { // eslint-disable-line no-unused-vars
+    try {
+        const fileName = await writeSnapshot(`./${Date.now()}.heapsnapshot`);
+        message.channel.send(`Created new heap-dump @ \`${fileName}\`.`);
+    } catch (e) {
+        this.client.logger.error(e.stack);
+        message.channel.send(`An error occurred.\n\`\`\`${error.message}\`\`\``);
+    }
+  }
+}
+
+module.exports = HeapDump;
