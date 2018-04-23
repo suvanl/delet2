7e3181e8e4715c1389032f96f632224e55648689
Suvan <mr.slrmx@gmail.com>
2018-04-23 16:21:20 +0100

    Add new fields to output (stats.js)

--- commands/Information/stats.js
+++ commands/Information/stats.js
@@ -17,14 +17,34 @@ class Stats extends Command {
 
   async run(message, args, level) { // eslint-disable-line no-unused-vars
     const duration = moment.duration(this.client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
+    
+    let platform;
+    let build;
+
+    if (process.platform === "win32") {
+      platform = "windows";
+      build = "Development";
+    } else if (process.platform === "linux") {
+      platform = process.platform;
+      build = "Production";
+      // Note that the above two `if`/`else if` statements are specific to delet, as
+      // delet's development build runs on Windows, and delet's production server runs Linux.
+    } else {
+      platform = process.platform;
+      build = "Unknown";
+    }
+    
     message.channel.send(`= STATISTICS =
   • Users      :: ${this.client.users.size.toLocaleString()}
   • Servers    :: ${this.client.guilds.size.toLocaleString()}
   • Channels   :: ${this.client.channels.size.toLocaleString()}
   • Uptime     :: ${duration}
   • RAM usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
-  • Version    :: v${pkg.version}
-  • Platform   :: ${process.platform.toProperCase()}
+  • Build      :: ${build}
+  • Platform   :: ${platform.toProperCase()}
+
+= VERSIONS =
+  • delet      :: v${pkg.version}
   • Discord.js :: v${version}
   • Node.js    :: ${process.version}`, {code: "asciidoc"});
   }
