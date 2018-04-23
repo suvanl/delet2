46685a1edd7526b334bb3bcc727c3f782bfc039a
Suvan <mr.slrmx@gmail.com>
2018-04-23 15:36:41 +0100

    Move stats command to "Information" category

--- commands/Information/stats.js
+++ commands/Information/stats.js
@@ -1,4 +1,5 @@
 const Command = require("../../base/Command.js");
+const pkg = require("../../package.json");
 const { version } = require("discord.js");
 const moment = require("moment");
 require("moment-duration-format");
@@ -7,8 +8,8 @@ class Stats extends Command {
   constructor(client) {
     super(client, {
       name: "stats",
-      description: "Gives some useful bot statistics.",
-      category: "System",
+      description: "Sends some useful bot statistics.",
+      category: "Information",
       usage: "stats",
       aliases: ["statistics"]
     });
@@ -17,11 +18,13 @@ class Stats extends Command {
   async run(message, args, level) { // eslint-disable-line no-unused-vars
     const duration = moment.duration(this.client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
     message.channel.send(`= STATISTICS =
-  • RAM usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
-  • Uptime     :: ${duration}
   • Users      :: ${this.client.users.size.toLocaleString()}
   • Servers    :: ${this.client.guilds.size.toLocaleString()}
   • Channels   :: ${this.client.channels.size.toLocaleString()}
+  • Uptime     :: ${duration}
+  • RAM usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
+  • Version    :: v${pkg.version}
+  • Platform   :: ${process.platform.toProperCase()}
   • Discord.js :: v${version}
   • Node.js    :: ${process.version}`, {code: "asciidoc"});
   }
