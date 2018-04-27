cd581f7ca4d9291728b45ef549f30c9a1586e215
Suvan <mr.slrmx@gmail.com>
2018-04-27 23:03:15 +0100

    Add new alias to userinfo command

--- commands/Information/userinfo.js
+++ commands/Information/userinfo.js
@@ -9,7 +9,7 @@ class UserInfo extends Command {
         description: "Displays information about the mentioned user.",
         category: "Information",
         usage: "userinfo [user]",
-        aliases: ["uinfo", "user"],
+        aliases: ["uinfo", "user", "whois"],
         guildOnly: true
       });
     }
