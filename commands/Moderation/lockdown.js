05870f1c77054d222cd6abc3e394d8326075ae43
Suvan <mr.slrmx@gmail.com>
2018-05-07 22:57:16 +0100

    Lockdown system improvements

--- commands/Moderation/lockdown.js
+++ commands/Moderation/lockdown.js
@@ -1,4 +1,5 @@
 const Command = require("../../base/Command.js");
+const { RichEmbed } = require("discord.js");
 const ms = require("ms");
 
 class Lockdown extends Command {
@@ -15,12 +16,15 @@ class Lockdown extends Command {
     }
 
     async run(message, args, level) { // eslint-disable-line no-unused-vars
-        const settings = message.guild ? this.client.getSettings(message.guild.id) : this.client.settings.get("default");
-
         if (!message.guild.available) return this.client.logger.info(`Guild "${message.guild.name}" (${message.guild.id}) is unavailable.`);
+
+        const settings = message.guild ? this.client.getSettings(message.guild.id) : this.client.settings.get("default");
+        const modLog = message.guild.channels.find("name", settings.modLogChannel);
+        if (!modLog) return message.channel.send(`Modlog channel not found. If you're an admin (or owner) on this server, please use:\`\`\`${settings.prefix}set edit modLogChannel {{channel name}}\`\`\`\nFor example: \`${settings.prefix}set edit modLogChannel cool-channel-name\`.`);
+
         if (!this.client.lockit) this.client.lockit = [];
         const time = args.join(" ");
-        const validUnlocks = ["release", "unlock"];
+        const validUnlocks = ["release", "rel", "unlock", "end", "stop"];
         if (!time) return message.channel.send("A duration for the lockdown must be set. This can be in hours, minutes or seconds. Example command usage:\n```%lockdown 5 m```");
 
         try {
@@ -36,7 +40,13 @@ class Lockdown extends Command {
                 message.channel.overwritePermissions(message.guild.id, {
                     SEND_MESSAGES: false
                 }).then(() => {
-                    message.channel.send(`Channel locked down for ${ms(ms(time), { long: true })}.`).then (() => {
+                    const embed = new RichEmbed()
+                        .setTitle("🔒 Channel locked down")
+                        .setColor(16753762)
+                        .setDescription(`\`\`\`ruby\nChannel: #${message.channel.name} (${message.channel.id})\nDuration: ${ms(ms(time), { long: true })}\nIssued by: ${message.author.tag}\`\`\``)
+                        .setFooter("Moderation system powered by delet", this.client.user.displayAvatarURL)
+                        .setTimestamp();
+                    this.client.channels.get(modLog.id).send({embed}).then (() => {
                         this.client.lockit[message.channel.id] = setTimeout(() => {
                             message.channel.overwritePermissions(message.guild.id, {
                                 SEND_MESSAGES: null
