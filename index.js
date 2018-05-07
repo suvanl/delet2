7a272d0e62e06999bf6c975b2893149c80228197
Suvan <mr.slrmx@gmail.com>
2018-05-07 20:29:50 +0100

    Set `disableEveryone` client option to true

--- index.js
+++ index.js
@@ -144,7 +144,7 @@ class Delet extends Discord.Client {
 // This is the client. Can sometimes be called `bot` or `self` too,
 // in other bots. Either way, `client.something`, or `bot.something`,
 // is used, this is what is being referred to - the client.
-const client = new Delet();
+const client = new Delet({disableEveryone: true});
 console.log(client.config.permLevels.map(p => `${p.level} : ${p.name}`));
 
 // Starts by getting some useful functions that will be used
