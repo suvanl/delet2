90b961634191f4a1f6cbb53ea2a25617247158a6
Suvan <mr.slrmx@gmail.com>
2018-05-08 10:05:09 +0100

    Move position of `lastMessage` constant declaration (kick.js)

--- commands/Moderation/kick.js
+++ commands/Moderation/kick.js
@@ -21,7 +21,6 @@ class Kick extends Command {
       const user = message.mentions.users.first();
       const reason = args.slice(1).join(" ");
       const modLog = message.guild.channels.find("name", settings.modLogChannel);
-      const lastMessage = message.guild.member(user).lastMessageID;
       if (!modLog) return message.channel.send(`Modlog channel not found. If you're an admin (or owner) on this server, please use:\`\`\`${settings.prefix}set edit modLogChannel {{channel name}}\`\`\`\nFor example: \`${settings.prefix}set edit modLogChannel cool-channel-name\`.`);
       if (!user) return message.channel.send("You must mention a user to kick.");
       if (!reason) return message.channel.send("You must provide a reason for the punishment.");
@@ -34,6 +33,8 @@ class Kick extends Command {
       } catch (error) {
         return message.channel.send("An error occurred whilst trying to kick the mentioned user.");
       }
+
+      const lastMessage = message.guild.member(user).lastMessageID;
 
       const embed = new RichEmbed()
       .setTitle(`👢 Member kicked in #${message.channel.name}`)
