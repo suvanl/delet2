8932817069caf220ee6f7db0c2c6687b8fb4abbd
Suvan <mr.slrmx@gmail.com>
2018-05-08 10:23:36 +0100

    Add new global text

--- util/globals.js
+++ util/globals.js
@@ -1,1 +1,2 @@
 exports.modLogNotFound = "Modlog channel not found. If you're an admin (or owner) on this server, please use:```{{prefix}}set edit modLogChannel [channel-name]```\nFor example: `{{prefix}}set edit modLogChannel cool-channel-name`.";
+exports.poweredBy = "Moderation system powered by delet";
