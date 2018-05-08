59bce4c40656952a6086ff19b4ca66fae1c91539
Suvan <mr.slrmx@gmail.com>
2018-05-08 09:58:10 +0100

    Temporarily remove errors.js

--- util/errors.js
+++
@@ -1,4 +0,0 @@
-exports.NOTBOOL = "Invalid data type provided.\nThe value must be a boolean, i.e. \"True\" or \"False\".";
-exports.NaN = "";
-exports.NOTSTRING = "Invalid data type provided.\nThe value must be a string (text).",
-exports.UNDEFINED = "Null or undefined value provided.";
