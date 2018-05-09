e3904121982bf3366894c1b3b89a4f2889f9c871
Suvan <mr.slrmx@gmail.com>
2018-05-09 22:52:36 +0100

    Create `missingPerm` message

--- util/globals.js
+++ util/globals.js
@@ -1,3 +1,4 @@
 exports.modLogNotFound = "Modlog channel not found. If you're an admin (or owner) on this server, please use:```{{prefix}}set edit modLogChannel [channel-name]```\nFor example: `{{prefix}}set edit modLogChannel cool-channel-name`.";
 exports.poweredBy = "Moderation system powered by delet";
 exports.error = "An error occurred:\n";
+exports.missingPerm = "I cannot run this command as I have insufficient permissions to do so. Please ensure I have the \"{{perm}}\" permission.";
