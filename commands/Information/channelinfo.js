4924bbb35cf46a84a821f50a263be8a671f7b437
Suvan <mr.slrmx@gmail.com>
2018-05-11 22:27:22 +0100

    Create channelinfo command

---
+++ commands/Information/channelinfo.js
@@ -0,0 +1,38 @@
+const Command = require("../../base/Command.js");
+const { RichEmbed } = require("discord.js");
+const moment = require("moment");
+
+class ChannelInfo extends Command {
+    constructor(client) {
+      super(client, {
+        name: "channelinfo",
+        description: "Displays information about the specified channel.",
+        category: "Information",
+        usage: "channelinfo [channel]",
+        aliases: ["channel", "cinfo"],
+        guildOnly: true
+      });
+    }
+
+    async run(message, args, level) { // eslint-disable-line no-unused-vars
+        let chan;
+        if (!args[0]) chan = message.channel;
+        else chan = this.client.channels.get(args[0]);
+
+        const createdTimestamp = moment.utc(chan.createdAt).format("YYYYMMDD");
+
+        const embed = new RichEmbed()
+            .setColor(7506394)
+            .setThumbnail("https://vgy.me/9fSC7k.png")
+            .setTitle(`Channel Information for #${chan.name}`)
+            .addField("Created", chan.createdAt, true)
+            .addField("Age", moment(createdTimestamp, "YYYYMMDD").fromNow().slice(0, -4), true)
+            .addField("Type", chan.type.toProperCase())
+            .setFooter(`Channel ID: ${chan.id}`)
+            .setTimestamp();
+
+        message.channel.send({embed});
+    }
+}
+
+module.exports = ChannelInfo;
