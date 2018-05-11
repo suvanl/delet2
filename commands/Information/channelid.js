3ccb4d3a71d5dd8f6218c0b410e54a9ebcde74ea
Suvan <mr.slrmx@gmail.com>
2018-05-11 22:27:15 +0100

    Create channelid command

---
+++ commands/Information/channelid.js
@@ -0,0 +1,25 @@
+const Command = require("../../base/Command.js");
+
+class ChannelID extends Command {
+    constructor(client) {
+      super(client, {
+        name: "channelid",
+        description: "Returns the ID of the current channel.",
+        category: "Information",
+        usage: "channelid",
+        aliases: ["channel-id", "cid"],
+        guildOnly: true
+      });
+    }
+
+    async run(message, args, level) { // eslint-disable-line no-unused-vars
+      let channelID;
+      
+      if (!args[0]) channelID = message.channel.id;
+      else channelID = this.client.channels.get(args[0].id);
+
+      message.channel.send(channelID);
+    }
+}
+
+module.exports = ChannelID;
