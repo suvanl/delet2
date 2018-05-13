8c392a35985cc13c8924832b67f5c064c7de142e
Suvan <mr.slrmx@gmail.com>
2018-05-13 22:12:47 +0100

    Update cat image API URL & improve error handling

--- commands/Fun/cat.js
+++ commands/Fun/cat.js
@@ -15,16 +15,12 @@ class Cat extends Command {
     }
 
     async run(message, args, level) { // eslint-disable-line no-unused-vars
-        const { body } = await snekfetch.get("https://random.cat/meow");
+        const { body } = await snekfetch.get("http://aws.random.cat/meow").catch(error => this.client.logger.error(error));
         try {
-            message.channel.send("Unfortunately, the cat API can sometimes be quite slow. Please wait for your image to be fetched, this is not an error.")
-                .then(msg => {
-                    msg.delete(10000);
-                });
             message.channel.send({files: [body.file] });
         } catch (error) {
             this.client.logger.error(error);
-            return message.channel.send(`${texts.error}${error.message}`);
+            return message.channel.send(`${texts.error}\`\`\`${error.message}\`\`\``);
         }
     }
 }
