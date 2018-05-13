0baff916b6bc4113cc05e8ccf439cab3dfc3a410
Suvan <mr.slrmx@gmail.com>
2018-05-13 21:23:58 +0100

    Update feedback command

--- commands/Information/feedback.js
+++ commands/Information/feedback.js
@@ -1,4 +1,5 @@
 const Command = require("../../base/Command.js");
+const delet = require("../../package.json");
 
 class Feedback extends Command {
     constructor(client) {
@@ -12,13 +13,13 @@ class Feedback extends Command {
     }
 
     async run(message, args, level) { // eslint-disable-line no-unused-vars
-        const sf = "<https://delet.js.org/suggest>";
-        const issues = "<https://github.com/DS-Development/delet/issues>";
+        const suggest = "<https://delet.js.org/suggest>";
+        const issues = `<${delet.bugs.url}>`;
 
         message.channel.send(`
 Want to suggest something, or give feedback? Encountering any bugs/issues?
 
-**Suggestions & feedback**: ${sf}.
+**Suggestions & feedback**: ${suggest}.
 **Bugs/issues**: ${issues}.
 `);
     }
