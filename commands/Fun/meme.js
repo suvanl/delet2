d56fe21083918423cd44229c442e685acf325c74
Suvan <mr.slrmx@gmail.com>
2018-05-14 18:24:56 +0100

    Create meme/me_irl command

---
+++ commands/Fun/meme.js
@@ -0,0 +1,23 @@
+const Command = require("../../base/Command.js");
+const meme = require("memejs");
+
+class Meme extends Command {
+    constructor(client) {
+      super(client, {
+        name: "meme",
+        description: "Sends a meme.",
+        category: "Fun",
+        usage: "meme",
+        aliases: ["memes", "me_irl", "me-irl"]
+      });
+    }
+
+    async run(message, args, level) { // eslint-disable-line no-unused-vars
+      meme("me_irl", function(data, err) {
+        if (err) return this.client.logger.error(err);
+        message.channel.send(data.url);
+      });
+    }
+}
+
+module.exports = Meme;
