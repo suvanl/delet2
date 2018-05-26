7195a49fb8ff33b52679777af0bcd3554ed93fb2
Suvan <mr.slrmx@gmail.com>
2018-05-26 11:09:34 +0100

    Use `Number.prototype.toLocaleString()` for maths command outputs

--- commands/Maths/add.js
+++ commands/Maths/add.js
@@ -11,13 +11,15 @@ class Add extends Command {
       });
     }
 
-    async run(message, args, level) { // eslint-disable-line no-unused-vars
+    async run(message, args, level, settings) { // eslint-disable-line no-unused-vars
+        const locale = settings.language;
+
         const numArray = args.map(n => parseInt(n));
         if (!args[1] || numArray.length < 2) return message.channel.send("You must provide at least 2 integer numbers to add.");
 
         const total = numArray.reduce((p, c) => p+c);
         
-        message.channel.send(total);
+        message.channel.send(total.toLocaleString(locale));
     }
 }
 
