260baf09525769211323ec6077f81ea344e8877f
Suvan <mr.slrmx@gmail.com>
2018-05-28 19:59:49 +0100

    Create Base64 util

---
+++ util/Utils.js
@@ -0,0 +1,9 @@
+class Utils {
+    static base64(text, mode = "encode") {
+        if (mode === "encode") return Buffer.from(text).toString("base64");
+        if (mode === "decode") return Buffer.from(text, "base64").toString("utf8") || null;
+        throw new TypeError(`${mode} is not a supported Base64 mode`);
+    }
+}
+
+module.exports = Utils;
