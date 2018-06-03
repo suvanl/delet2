a6cc86da396e0e418dd90e78a287ee63b0dfe7bf
Suvan <mr.slrmx@gmail.com>
2018-06-03 17:45:20 +0100

    Create uptime command

---
+++ commands/Information/uptime.js
@@ -0,0 +1,26 @@
+const Command = require("../../base/Command.js");
+const { stripIndents } = require("common-tags");
+const moment = require("moment");
+require("moment-duration-format");
+
+class Uptime extends Command {
+    constructor(client) {
+      super(client, {
+        name: "uptime",
+        description: "Returns delet's uptime.",
+        category: "Information",
+        usage: "uptime"
+      });
+    }
+
+    async run(message, args, level, settings) { // eslint-disable-line no-unused-vars
+        const uptime = moment.duration(this.client.uptime).format("D [days], H [hrs], m [mins], s [secs]");
+        message.channel.send(stripIndents`
+        \`\`\`asciidoc
+        • Uptime     :: ${uptime}
+        \`\`\`
+        For a full list of bot stats, use the \`${settings.prefix}stats\` command.`);
+    }
+}
+
+module.exports = Uptime;
