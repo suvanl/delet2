dffb8674ffea3fcd37a096fabfc0578613abeb6a
Suvan <mr.slrmx@gmail.com>
2018-06-12 21:55:03 +0100

    Create `hash` static method in Utils class

--- util/Utils.js
+++ util/Utils.js
@@ -1,8 +1,14 @@
+const crypto = require("crypto");
+
 class Utils {
     static base64(text, mode = "encode") {
         if (mode === "encode") return Buffer.from(text).toString("base64");
         if (mode === "decode") return Buffer.from(text, "base64").toString("utf8") || null;
         throw new TypeError(`${mode} is not a supported Base64 mode`);
+    }
+    
+    static hash(text, algorithm) {
+        return crypto.createHash(algorithm).update(text).digest("hex");
     }
 }
 
