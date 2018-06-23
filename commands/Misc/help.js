b081271aff8dba88f9cd52d9fb213ed4b0b6776b
Suvan <mr.slrmx@gmail.com>
2018-06-23 16:53:41 +0100

    Update help.js

--- commands/Misc/help.js
+++ commands/Misc/help.js
@@ -49,7 +49,7 @@ class Help extends Command {
 
       // Sends the output to the message author, and catches any errors that occur
         message.channel.send(`${message.author}, sending a list of commands available for your permission level to your DMs... 📝`);
-        message.author.send(output, {code:"asciidoc", split: { char: "\u200b" }}).catch(e => {
+        message.author.send(output, { code:"asciidoc", split: { char: "\u200b" } }).catch(e => {
           if (e.toString().startsWith("DiscordAPIError: Cannot send messages to this user")) {
             return message.channel.send(`Oops, looks like the message didn't make it to your DMs, ${message.author}. Please ensure "**Allow direct messages from server members**" is on in your privacy settings for this server.`, {
               file: image
