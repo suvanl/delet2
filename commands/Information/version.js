072f4e4c24978492325ad64a51014b4af5c28381
Suvan <mr.slrmx@gmail.com>
2018-06-24 12:30:38 +0100

    Create version command

---
+++ commands/Information/version.js
@@ -0,0 +1,29 @@
+const Command = require("../../base/Command.js");
+const moment = require("moment");
+const snekfetch = require("snekfetch");
+const { version } = require("../../package.json");
+
+class Version extends Command {
+    constructor(client) {
+      super(client, {
+        name: "version",
+        description: "Returns delet's version.",
+        category: "Information",
+        usage: "version",
+        aliases: ["ver"]
+      });
+    }
+
+    async run(message, args, level, settings) { // eslint-disable-line no-unused-vars
+        try {
+            var { body } = await snekfetch.get("https://api.github.com/repos/DS-Development/delet");
+        } catch (error) {
+            this.client.logger.error(error);
+            body = "[Error occurred whilst fetching date]";
+        }
+        
+        message.channel.send(`**Version:** ${version}\n**Last updated:** ${moment.utc(body.updated_at, "YYYYMMDD").fromNow() || body}`);
+    }
+}
+
+module.exports = Version;
