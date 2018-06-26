ee02096c2f1e53dfec377b1e7cfe0b88769f0c34
Suvan <mr.slrmx@gmail.com>
2018-06-26 14:30:43 +0100

    Use Promise.prototype.catch() method rather than try/catch

--- commands/Moderation/bans.js
+++ commands/Moderation/bans.js
@@ -16,19 +16,18 @@ class Bans extends Command {
     async run(message, args, level, settings, texts) { // eslint-disable-line no-unused-vars
         if (!message.guild.available) return this.client.logger.info(`Guild "${message.guild.name}" (${message.guild.id}) is unavailable.`);
         if (!message.guild.me.hasPermission("BAN_MEMBERS")) return message.channel.send(texts.general.missingPerm.replace(/{{perm}}/g, "Ban Members"));
-        try {
-            message.guild.fetchBans()
-                .then(bans => {
-                    let plurality;
-                    if (bans.size === 1) plurality = "user";
-                    else plurality = "users";
+        message.guild.fetchBans()
+            .then(bans => {
+                let plurality;
+                if (bans.size === 1) plurality = "user";
+                else plurality = "users";
 
-                    message.channel.send(`This server has **${bans.size}** banned ${plurality}.`);
-                });
-        } catch (error) {
-            this.client.logger.error(error);
-            return message.channel.send(texts.general.error.replace(/{{err}}/g, error.message));
-        }
+                message.channel.send(`This server has **${bans.size}** banned ${plurality}.`);
+            })
+            .catch(error => {
+                this.client.logger.error(error);
+                return message.channel.send(texts.general.error.replace(/{{err}}/g, error.message));
+            });
     }
 }
 
