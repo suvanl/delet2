7988c511af0340d972d1e7079c369e2762567220
Suvan <mr.slrmx@gmail.com>
2018-07-08 16:32:52 +0100

    Create new guildMemberRemove event

---
+++ events/guildMemberRemove.js
@@ -0,0 +1,32 @@
+// This event executes when a member leaves a server.
+
+module.exports = class {
+  constructor(client) {
+    this.client = client;
+  }
+
+  async run(member) {
+    // Return if guild is unavailable
+    if (!member.guild.available) return;
+
+    // Loads the guild's settings
+    const settings = this.client.getSettings(member.guild.id);
+
+    // TODO: re-add leave message to settings and handle sending of said message in this event
+
+    // Checks if the modLogChannel exists
+    const modLog = member.guild.channels.find("name", settings.modLogChannel);
+    if (!modLog) return;
+
+    // Creates and sends embed
+    const { RichEmbed } = require("discord.js");
+    const embed = new RichEmbed()
+    .setColor(16767063)
+        .setTitle("🚪 User Left")
+        .setDescription(`**${member.user.tag}** (${member.user.id})`)
+        .setFooter(`There are now ${member.guild.memberCount} members`)
+        .setTimestamp();
+    return member.guild.channels.get(modLog.id).send({embed});
+  }
+};
+  
