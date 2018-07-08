411e093986003e8a5f8d84f72a71e91ed5f1610a
Suvan <mr.slrmx@gmail.com>
2018-07-08 17:46:51 +0100

    Create faketweet command

---
+++ commands/Fun/faketweet.js
@@ -0,0 +1,35 @@
+const Command = require("../../base/Command.js");
+const snekfetch = require("snekfetch");
+
+class FakeTweet extends Command {
+    constructor(client) {
+      super(client, {
+        name: "faketweet",
+        description: "Creates a fake tweet.",
+        category: "Fun",
+        usage: "faketweet [username] [message]",
+        aliases: ["fake-tweet"]
+      });
+    }
+
+    async run(message, args, level, settings, texts) { // eslint-disable-line no-unused-vars
+      const user = args[0];
+      if (!user) return message.channel.send("You must provide a Twitter username, to have as the author of the tweet.");
+
+      const text = await this.client.awaitReply(message, "Please enter the tweet's message...\nReply with `cancel` to exit this text-entry period.", 30000);
+      if (text.toLowerCase() === "cancel") return message.channel.send("Cancelled.");
+
+      const msg = await message.channel.send("Generating...");
+
+      try {
+        const { body } = await snekfetch.get(`https://nekobot.xyz/api/imagegen?type=tweet&username=${user}&text=${encodeURIComponent(text)}`);
+        message.channel.send("", { file: body.message });
+        msg.edit("Done!");
+      } catch (error) {
+        this.client.logger.error(error);
+        return message.channel.send(texts.general.error.replace(/{{err}}/g, error.message));
+      }
+    }
+}
+
+module.exports = FakeTweet;
