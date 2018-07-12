498526369a360715e899abbcad13e1754fda6951
Suvan <mr.slrmx@gmail.com>
2018-07-12 12:53:44 +0100

    Add locale support to EmojiImage command

--- commands/Information/emojiimage.js
+++ commands/Information/emojiimage.js
@@ -12,16 +12,16 @@ class EmojiImage extends Command {
       });
     }
 
-    async run(message, args, level) { // eslint-disable-line no-unused-vars
-        if (!args[0]) return message.channel.send("You must provide an emoji for me to look up.");
-        if (args[0].charCodeAt(0) >= 55296) return message.channel.send(`${args[0]} is a regular Discord emoji, from Twemoji.\nhttps://twitter.github.io/twemoji/`);
+    async run(message, args, level, settings, texts) { // eslint-disable-line no-unused-vars
+        if (!args[0]) return message.channel.send(texts.cmd.noEmoji);
+        if (args[0].charCodeAt(0) >= 55296) return message.channel.send(`${texts.cmd.regularEmoji.replace(/{{emoji}}/g, args[0])}\nhttps://twitter.github.io/twemoji/`);
 
         const match = args[0].match(/<:[a-zA-Z0-9_-]+:(\d{18})>/);
-        if (!match || !match[1]) return message.channel.send("You must provide a valid emoji.");
+        if (!match || !match[1]) return message.channel.send(texts.cmd.invalidEmoji);
 
         // Using the `emojis` property from Client rather than Guild so Nitro users can use this command for a wider range of emojis
         const emoji = this.client.emojis.get(match[1]);
-        if (!emoji) return message.channel.send("Invalid emoji.");
+        if (!emoji) return message.channel.send(texts.cmd.invalidEmoji);
 
         const embed = new RichEmbed()
             .setColor(2934736)
