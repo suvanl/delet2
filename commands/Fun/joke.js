8e62539391d57d315c693b3541eaab46e92c5f64
Suvan <mr.slrmx@gmail.com>
2018-07-25 21:29:31 +0100

    Create joke command

---
+++ commands/Fun/joke.js
@@ -0,0 +1,36 @@
+const Command = require("../../base/Command.js");
+const snekfetch = require("snekfetch");
+const { RichEmbed } = require("discord.js");
+
+class Joke extends Command {
+    constructor(client) {
+      super(client, {
+        name: "joke",
+        description: "Tells a general or programming-related joke.",
+        category: "Information",
+        usage: "joke",
+        aliases: ["humour", "humor"]
+      });
+    }
+
+    async run(message, args, level, settings, texts) { // eslint-disable-line no-unused-vars
+        const randomColor = "#0000".replace(/0/g, function() {
+          return (~~(Math.random() * 16)).toString(16);
+        });
+        
+        try {
+          const { body } = await snekfetch.get("https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke");
+          const embed = new RichEmbed()
+            .setColor(randomColor)
+            .setAuthor("Joke", "https://vgy.me/hLZJX4.png")
+            .setDescription(`${body.setup}\n*${body.punchline}*`)
+            .setFooter(`Category: ${body.type.toProperCase()}`);
+          return message.channel.send({ embed });
+        } catch (error) {
+          this.client.logger.error(error);
+          return message.channel.send(texts.general.error.replace(/{{err}}/g, error));
+        }
+    }
+}
+
+module.exports = Joke;
