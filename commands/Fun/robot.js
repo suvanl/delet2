12426ca02239d519bef1813a1af149e7833d776f
Suvan <mr.slrmx@gmail.com>
2018-07-25 23:26:38 +0100

    Create Robot command

---
+++ commands/Fun/robot.js
@@ -0,0 +1,28 @@
+const Command = require("../../base/Command.js");
+const { get } = require("snekfetch");
+
+class Robot extends Command {
+    constructor(client) {
+      super(client, {
+        name: "robot",
+        description: "Generates a picture of a robot from some given text.",
+        usage: "robot [text]",
+        aliases: ["robohash"]
+      });
+    }
+
+    async run(message, args, level, settings, texts) { // eslint-disable-line no-unused-vars
+        const query = args.join(" ");
+        if (!query) return message.channel.send("You must some text to use to generate the robot.");
+
+        try {
+          const { raw } = await get(`https://robohash.org/${query}.png`);
+          message.channel.send(`*"${query}"*`, { file: raw });
+        } catch (error) {
+          this.client.logger.error(error);
+          return message.channel.send(texts.general.error.replace(/{{err}}/g, error.message));
+        }
+    }
+}
+
+module.exports = Robot;
