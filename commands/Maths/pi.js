6b08d26f6a2de126c637fc7d08e416ee245868bb
Suvan <mr.slrmx@gmail.com>
2018-07-26 11:45:05 +0100

    Update Pi command

--- commands/Maths/pi.js
+++ commands/Maths/pi.js
@@ -12,12 +12,7 @@ class Pi extends Command {
     }
 
     async run(message, args, level, settings) { // eslint-disable-line no-unused-vars
-        if (message.content.startsWith(`${settings.prefix}π`)) {
-            return message.channel.send(`π = **${Math.PI}**...`);
-        }
-        const msg = await message.channel.send(`π = **${Math.PI}**...`);
-        await this.client.wait(500);
-        msg.edit(`π = **${Math.PI}**...\n\nFind out more about Pi here: **<https://en.wikipedia.org/wiki/Pi>**.\nOne million digits of Pi: **<https://www.piday.org/million/>**.`);
+        message.channel.send(`π = **${Math.PI}**...`);
     }
 }
 
