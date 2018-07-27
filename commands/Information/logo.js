51881f3716448860c43fd2fb8ef91e8273e7c544
Suvan <mr.slrmx@gmail.com>
2018-07-27 12:27:44 +0100

    Create logo command

---
+++ commands/Information/logo.js
@@ -0,0 +1,30 @@
+const Command = require("../../base/Command.js");
+const { get } = require("snekfetch");
+
+class Logo extends Command {
+    constructor(client) {
+      super(client, {
+        name: "logo",
+        description: "Sends a website's logo.",
+        category: "Information",
+        usage: "logo [url]"
+      });
+    }
+
+    async run(message, args, level, settings, texts) { // eslint-disable-line no-unused-vars
+        const query = args[0];
+        if (!query) return message.channel.send(texts.general.invalidURL);
+
+        try {
+            const { raw } = await get(`https://logo.clearbit.com/${query}?size=500`);
+            message.channel.send("", { files: [{ attachment: raw, name: "logo.jpg" }] });
+        } catch (error) {
+            if (error.message === "400 Bad Request") return message.channel.send(texts.general.invalidURL);
+            if (error.message === "404 Not Found") return message.channel.send(texts.general.noResultsFound);
+            this.client.logger.error(error);
+            return message.channel.send(texts.general.error.replace(/{{err}}/g, error.message));
+        }
+    }
+}
+
+module.exports = Logo;
