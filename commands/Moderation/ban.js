a374606effbb8ad8af46cd87fcc1a14ad0a6abdd
Suvan <mr.slrmx@gmail.com>
2018-07-31 13:56:01 +0100

    Add locale support to ban command

--- commands/Moderation/ban.js
+++ commands/Moderation/ban.js
@@ -21,34 +21,34 @@ class Ban extends Command {
         let reason = args.slice(1).join(" ") || undefined;
         const modLog = message.guild.channels.find("name", settings.modLogChannel);
         if (!modLog) return message.channel.send(texts.moderation.modLogNotFound.replace(/{{prefix}}/g, settings.prefix));
-        if (!user) return message.channel.send("You must mention a user to ban.");
-        if (user === message.author) return message.channel.send("You cannot ban yourself. <a:aThinking:444074885367595009>");
-        if (message.guild.member(message.author).highestRole.position <= message.guild.member(user).highestRole.position) return message.channel.send("You cannot ban this user as they have a higher role than you.");
+        if (!user) return message.channel.send(texts.moderation.noUser);
+        if (user === message.author) return message.channel.send(`${texts.moderation.selfPunish} <a:aThinking:444074885367595009>`);
+        if (message.guild.member(message.author).highestRole.position <= message.guild.member(user).highestRole.position) return message.channel.send(texts.moderation.insufficientRole);
         if (!reason) {
-          message.channel.send("Please enter a reason for the punishment...\nThis text-entry period will time-out in 30 seconds. Reply with `cancel` to exit.");
+          message.channel.send(texts.moderation.awaitReason);
           await message.channel.awaitMessages(m => m.author.id === message.author.id, {
             "errors": ["time"],
             "max": 1,
             time: 30000
           }).then(resp => {
-            if (!resp) return message.channel.send("Timed out. The user has not been banned.");
+            if (!resp) return message.channel.send(texts.moderation.timedOut);
             resp = resp.array()[0];
-            if (resp.content.toLowerCase() === "cancel") return message.channel.send("Cancelled. The user has not been banned.");
+            if (resp.content.toLowerCase() === "cancel") return message.channel.send(texts.moderation.cancel);
             reason = resp.content;
             if (resp) resp.react("✅");
           }).catch(error => { // eslint-disable-line no-unused-vars
-            message.channel.send("Timed out. The user has not been banned.");
+            message.channel.send(texts.moderation.timedOut);
           });
         }
 
         if (reason) {
           try {
-            if (!message.guild.member(user).bannable) return message.reply("I cannot ban that user from this server!\nThis may be because I do not have the required permissions to do so, or they may be the server owner.");
+            if (!message.guild.member(user).bannable) return message.reply(texts.moderation.insufficientPerms);
             try {
-              message.guild.member(user).ban(`${reason} (Issued by ${message.author.tag})`);
+              message.guild.member(user).ban(`${reason} (${texts.moderation.issuedBy.replace(/{{user}}/g, message.author.tag)})`);
               message.react("👌");
             } catch (error) {
-              return message.channel.send("An error occurred whilst trying to ban the mentioned user");
+              return message.channel.send(texts.general.error.replace(/{{err}}/g, error.message));
             }
 
             const lastMessage = message.guild.member(user).lastMessageID;
@@ -60,7 +60,7 @@ class Ban extends Command {
               .setFooter(texts.moderation.poweredBy, this.client.user.displayAvatarURL)
               .setTimestamp();
     
-            this.client.channels.get(modLog.id).send({embed});
+            this.client.channels.get(modLog.id).send({ embed });
 
           } catch (error) {
             this.client.logger.error(error);
