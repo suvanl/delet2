6ad94bb8fef40dcd0f9741d1eda81c6f86e99449
Suvan <mr.slrmx@gmail.com>
2018-07-31 13:37:14 +0100

    Add "echo" alias to say command and allow lowercase command usage

--- commands/Fun/say.js
+++ commands/Fun/say.js
@@ -7,14 +7,14 @@ class Say extends Command {
         description: "Repeats your message.",
         category: "Fun",
         usage: "say [message]",
-        aliases: ["repeat"]
+        aliases: ["repeat", "echo"]
       });
     }
 
     async run(message, args, level, settings) { // eslint-disable-line no-unused-vars
       const ttsArgs = message.content.split(" ").slice(2);
 
-      if (message.content.startsWith(`${settings.prefix}say tts`)) {
+      if (message.content.toLowerCase().startsWith(`${settings.prefix}say tts`)) {
         message.channel.send(ttsArgs.join(" "), { tts: true });
       } else {
         message.channel.send(args.join(" "));
