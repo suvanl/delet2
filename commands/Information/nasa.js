26f15e35df13531fd24a86d856f1ceeb406a7fcc
Suvan <mr.slrmx@gmail.com>
2018-08-06 18:44:31 +0100

    Update NASA command

--- commands/Information/nasa.js
+++ commands/Information/nasa.js
@@ -1,6 +1,6 @@
 const Command = require("../../base/Command.js");
 const snekfetch = require("snekfetch");
-const { RichEmbed } = require("discord.js");
+const h = new (require("html-entities").AllHtmlEntities)();
 
 class NASA extends Command {
   constructor(client) {
@@ -14,11 +14,13 @@ class NASA extends Command {
 
   async run(message, args, level, settings, texts) { // eslint-disable-line no-unused-vars
     const query = args[0];
+    if (!query) return message.channel.send("You must provide a query to search NASA's image database for.");
+
     try {
       const { body } = await snekfetch
         .get("https://images-api.nasa.gov/search")
         .query({
-          q: query,
+          q: encodeURIComponent(query),
           media_type: "image"
         });
       
@@ -26,14 +28,9 @@ class NASA extends Command {
       if (!images.length || images.length === 0) return message.channel.send("No results found.");
 
       const data = images.random();
-      const description = data.data[0].description;
+      const description = h.decode(data.data[0].description);
 
-      const embed = new RichEmbed()
-        .setAuthor("NASA", "https://vgy.me/44d5fk.png")
-        .setDescription(description.length > 200 ? description.substring(0, 200) + "..." : description.substring(0, 200))
-        .setImage(data.links[0].href) // TODO: fix images not actually displaying within the embed
-        .setTimestamp();
-      return message.channel.send({ embed });
+      message.channel.send(description.length > 1997 ? description.substring(0, 1997) + "..." : description.substring(0, 2000) + "<:NASA:476079744857931796>", { file: data.links[0].href });
     } catch (error) {
       this.client.logger.error(error);
       message.channel.send(texts.general.error.replace(/{{err}}/g, error));
