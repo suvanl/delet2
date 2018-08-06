91af75a363e536880748806d3875fbca92af3d7e
Suvan <mr.slrmx@gmail.com>
2018-08-06 19:30:44 +0100

    Add missing permission check to invites command

--- commands/Misc/invites.js
+++ commands/Misc/invites.js
@@ -14,6 +14,8 @@ class Invites extends Command {
     }
 
     async run(message, args, level, settings, texts) { // eslint-disable-line no-unused-vars
+        if (!message.guild.me.hasPermission("MANAGE_GUILD")) return message.channel.send(texts.general.missingPerm.replace(/{{perm}}/g, "Manage Server"));
+        
         let invites = await message.guild.fetchInvites();
         if (invites.size === 0) return message.channel.send(texts.cmd.misc.noInvites);
         invites = invites.array();
