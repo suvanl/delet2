969cad3faad69057d1f5cf62cd2094a3658fd755
Suvan <mr.slrmx@gmail.com>
2018-08-06 18:14:59 +0100

    Support non-embedded links in Logo command

--- commands/Information/logo.js
+++ commands/Information/logo.js
@@ -16,10 +16,10 @@ class Logo extends Command {
         if (!query) return message.channel.send(texts.general.invalidURL);
 
         try {
-            const { raw } = await get(`https://logo.clearbit.com/${query}?size=500`);
+            const { raw } = await get(`https://logo.clearbit.com/${query.startsWith("<") ? query.replace(/<(.+)>/g, "$1") : query}?size=500`);
             message.channel.send("", { files: [{ attachment: raw, name: "logo.jpg" }] });
         } catch (error) {
-            if (error.message === "400 Bad Request") return message.channel.send(texts.general.invalidURL);
+            if (error.message === "400 Bad Request") return message.channel.send("You must provide a URL for me to return a logo for.");
             if (error.message === "404 Not Found") return message.channel.send(texts.general.noResultsFound);
             this.client.logger.error(error);
             return message.channel.send(texts.general.error.replace(/{{err}}/g, error.message));
