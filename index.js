4b1b02ee210dfde74c5c768d32d71612a8800117
Suvan <mr.slrmx@gmail.com>
2018-08-14 14:10:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -201,6 +201,6 @@ init();
 
 client.on("disconnect", () => client.logger.warn("Bot is disconnecting..."))
       .on("reconnect", () => client.logger.log("Bot reconnecting...", "log"))
-      .on("rateLimit", rateLimitInfo => client.logger.warn(`Rate limit hit:\nHTTP method: ${rateLimitInfo.method}\n${Object.entries(rateLimitInfo)}`))
+      // .on("rateLimit", rateLimitInfo => client.logger.warn(`Rate limit hit:\nHTTP method: ${rateLimitInfo.method}\n${Object.entries(rateLimitInfo)}`))
       .on("error", e => client.logger.error(e))
       .on("warn", info => client.logger.warn(info));
