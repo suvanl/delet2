00ece744b2b9779496af14b5252b7fc3c5e59903
Suvan <mr.slrmx@gmail.com>
2018-09-02 17:54:23 +0100

    Update unmute command

--- commands/Moderation/unmute.js
+++ commands/Moderation/unmute.js
@@ -1,5 +1,6 @@
 const Command = require("../../base/Command.js");
 const { RichEmbed } = require("discord.js");
+const { stripIndents } = require("common-tags");
 
 class Unmute extends Command {
   constructor(client) {
@@ -14,33 +15,73 @@ class Unmute extends Command {
   }
 
   async run(message, args, level, settings, texts) { // eslint-disable-line no-unused-vars
+    if (!message.guild.available) return this.client.logger.info(`Guild "${message.guild.name}" (${message.guild.id}) is unavailable.`);
+    if (!message.guild.me.hasPermission("MANAGE_ROLES")) return message.channel.send(texts.general.missingPerm.replace(/{{perm}}/g, "Manage Roles"));
+
+    const muteRole = message.guild.roles.find(role => role.name === "Muted");
+    const empty = await this.isEmpty(muteRole);
+    if (empty) return message.channel.send(`A "Muted" role does not exist on this server. To create one, please run the \`${settings.prefix}mute\` command.`);
+
     const user = message.mentions.users.first();
-    const reason = args.slice(1).join(" ");
+    let reason = args.slice(1).join(" ");
     const modLog = message.guild.channels.find(c => c.name === settings.modLogChannel);
     if (!modLog) return message.channel.send(texts.moderation.modLogNotFound.replace(/{{prefix}}/g, settings.prefix));
     if (!user) return message.channel.send("You must mention a user to unmute.");
-    if (!reason) return message.channel.send("Please provide a reason.");
+    if (user.id === message.author.id) return message.reply("you cannot unmute yourself!");
+    if (message.guild.member(message.author).highestRole.position <= message.guild.member(user).highestRole.position) return message.channel.send("You cannot unmute this user as they have a higher role than you.");
 
-    const muteRole = message.guild.roles.find(role => role.name === "Muted");
-
-    if (!message.guild.me.hasPermission("MANAGE_ROLES")) return message.channel.send("I do not have the required permission(s) to carry this out. Please ensure I have the \"Manage Roles\" permission.");
-
-    const embed = new RichEmbed()
-      .setTitle("🔊 Member unmuted")
-      .setColor(12451456)
-      .setDescription(`\`\`\`fix\nUser: ${user.tag} (${user.id})\nUndone by: ${message.author.tag} (${message.author.id})\nReason: ${reason}\`\`\``)
-      .setFooter(texts.moderation.poweredBy, this.client.user.displayAvatarURL)
-      .setTimestamp();
+    if (!reason) {
+      message.channel.send(texts.moderation.awaitReason);
+      await message.channel.awaitMessages(m => m.author.id === message.author.id, {
+        "errors": ["time"],
+        "max": 1,
+        time: 30000
+      }).then(resp => {
+        if (!resp) return message.channel.send(texts.moderation.timedOut);
+        resp = resp.array()[0];
+        if (resp.content.toLowerCase() === "cancel") return message.channel.send(texts.moderation.cancel);
+        reason = resp.content;
+        if (resp) resp.react("✅");
+      }).catch(() => {
+        message.channel.send(texts.moderation.timedOut);
+      });
+    }
 
     if (message.guild.member(user).roles.has(muteRole.id)) {
       message.guild.member(user).removeRole(muteRole).then(() => {
-          this.client.channels.get(modLog.id).send({ embed });
-          user.send(`You have been unmuted in **${message.guild.name}**. Please ensure you always follow the rules to prevent being muted again!`);
           message.react("👌");
       });
     } else {
       return message.reply("the mentioned user isn't muted, so I cannot unmute them.");
     }
+
+    try {
+      const embed = new RichEmbed()
+        .setTitle("🔊 Member unmuted")
+        .setColor(12451456)
+        .setDescription(stripIndents`
+        \`\`\`css
+          User: ${user.tag} (${user.id})
+          Undone by: ${message.author.tag} (${message.author.id})
+          Reason: ${reason}
+        \`\`\`
+        `)
+        .setFooter(texts.moderation.poweredBy, this.client.user.displayAvatarURL)
+        .setTimestamp();
+
+      message.guild.channels.get(modLog.id).send({ embed });
+      user.send(`You have been unmuted in **${message.guild.name}**.\nPlease ensure you always follow the rules to prevent being muted again!`);
+    } catch (error) {
+      this.client.logger.error(error.stack);
+      return message.channel.send(texts.general.error.replace(/{{err}}/g, error.message));
+    }
+  }
+
+  async isEmpty(obj) {
+    for (const key in obj) {
+      if (obj.hasOwnProperty(key)) return false;
+    }
+    return true;
   }
 }
 
