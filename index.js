ff791b216027d5b70941a320b6d2691096a85ff7
Suvan <32813609+suvanl@users.noreply.github.com>
2018-09-05 12:57:36 +0100

    Update Node version check comment (index.js)

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ https://delet.js.org/
 // MAIN ENTRY POINT
 
 // This will check if the Node.js version the system is running is the required
-// Node.js version (9.x.x) and if it isn't, it will throw the following error to say so.
+// version (8.x.x or above) and if it isn't, it will throw the following error to say so.
 if (process.version.slice(1).split(".")[0] < 8) throw new Error("Node.js 8.0.0 or higher is required. Update Node on your system.");
 
 // Loads up the Discord.js library
