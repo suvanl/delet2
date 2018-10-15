9902f51b7ca974a4ce7149af696f40b9127e61e2
Suvan <mr.slrmx@gmail.com>
2018-10-15 16:33:53 +0100

    Prevent time command erroring when no arguments are provided

--- commands/Information/time.js
+++ commands/Information/time.js
@@ -13,14 +13,20 @@ class Time extends Command {
     }
 
     async run(message, args, level, settings, texts) { // eslint-disable-line no-unused-vars
+        const link = "https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List";
         const timeZone = args.join("_").toUpperCase();
+        if (!timeZone) return message.channel.send(stripIndents`
+        You must provide a timezone to look up the time for.
+        For a full list of timezones, refer to the "TZ" column here: **<${link}>**.
+        `);
+
         try {
             const time = new Date().toLocaleTimeString("en-GB", { timeZone });
             return message.channel.send(`The time in **${timeZone}** is currently **${time}**.`);
         } catch (err) {
             message.channel.send(stripIndents`
             ${texts.general.error.replace(/{{err}}/g, err.message)}
-            For a full list of timezones, refer to the "TZ" column here: **<https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List>**.
+            For a full list of timezones, refer to the "TZ" column here: **<${link}>**.
 
             • Please ensure you are using the correct format, e.g. \`${settings.prefix}time europe/london\`.
             • Note that the continent of North America is split into **America** and **Canada**, e.g. \`${settings.prefix}time america/new york\`.
