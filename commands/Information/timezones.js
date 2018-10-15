28f93190b15e084291d8016bbcb815fe6a52ce86
Suvan <mr.slrmx@gmail.com>
2018-10-15 16:52:43 +0100

    Change codeblock format to `asciidoc` (timezones command)

--- commands/Information/timezones.js
+++ commands/Information/timezones.js
@@ -17,41 +17,41 @@ class Timezones extends Command {
 
     async run(message, args, level) { // eslint-disable-line no-unused-vars
         message.channel.send(stripIndents`= TIMEZONES =\n
-        #AFRICA
+        AFRICA
           • EAT     :: [${moment().tz("Africa/Nairobi").format("HH:mm ZZ")}]
           • CAT     :: [${moment().tz("Africa/Maputo").format("HH:mm ZZ")}]
           • WAT     :: [${moment().tz("Africa/Lagos").format("HH:mm ZZ")}]
           • WEST    :: [${moment().tz("Africa/Accra").format("HH:mm ZZ")}]
 
-        #AMERICA
+        AMERICA
           • EDT     :: [${moment().tz("America/New_York").format("HH:mm ZZ")}]
           • CDT     :: [${moment().tz("America/Chicago").format("HH:mm ZZ")}]
           • MDT     :: [${moment().tz("America/Denver").format("HH:mm ZZ")}]
           • AKDT    :: [${moment().tz("America/Anchorage").format("HH:mm ZZ")}]
           • UTC-10  :: [${moment().tz("Pacific/Honolulu").format("HH:mm ZZ")}]
 
-        #ASIA
+        ASIA
           • MVT     :: [${moment().tz("Indian/Maldives").format("HH:mm ZZ")}]
           • IST     :: [${moment().tz("Asia/Calcutta").format("HH:mm ZZ")}]
           • BTT     :: [${moment().tz("Asia/Dhaka").format("HH:mm ZZ")}]
           • ICT     :: [${moment().tz("Asia/Bangkok").format("HH:mm ZZ")}]
           • CST     :: [${moment().tz("Asia/Hong_Kong").format("HH:mm ZZ")}]
 
-        #EUROPE
+        EUROPE
           • UTC/GMT :: [${moment.utc().format("HH:mm ZZ")}]
           • BST     :: [${moment().tz("Europe/London").format("HH:mm ZZ")}]
           • CEST    :: [${moment().tz("Europe/Zurich").format("HH:mm ZZ")}]
           • EEST    :: [${moment().tz("Europe/Helsinki").format("HH:mm ZZ")}]
           • FET     :: [${moment().tz("Europe/Minsk").format("HH:mm ZZ")}]
 
-        #OCEANIA
+        OCEANIA
           • AWST    :: [${moment().tz("Australia/Perth").format("HH:mm ZZ")}]
           • ACST    :: [${moment().tz("Australia/Darwin").format("HH:mm ZZ")}]
           • AEST    :: [${moment().tz("Australia/Sydney").format("HH:mm ZZ")}]
           • NZST    :: [${moment().tz("Pacific/Auckland").format("HH:mm ZZ")}]
 
-        #OTHER
-          • UNIX    :: [${unix}]`, { code: "css" });
+        OTHER
+          • UNIX    :: [${unix}]`, { code: "asciidoc" });
     }
 }
 
