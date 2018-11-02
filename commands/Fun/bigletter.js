e9f2747bd013bc0be0019c2672981ab9939d4f1e
Suvan <mr.slrmx@gmail.com>
2018-11-02 23:41:21 +0000

    Bug fixes (bigletter command)

--- commands/Fun/bigletter.js
+++ commands/Fun/bigletter.js
@@ -13,14 +13,23 @@ class BigLetter extends Command {
 
     async run(message, args, level, settings, texts) { // eslint-disable-line no-unused-vars
       const generate = () => {
-        const text = args.join(" ").toLowerCase();
+        let text = args.join(" ");
         if (!text) return message.channel.send("You must provide some text to convert to regional indicator emojis.");
+        else text = args.join(" ").toLowerCase();
 
-        const pattern = /^[a-zA-Z0-9]+$/;
         const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-        const checkChars = pattern.test(text);
+        const isAlphaNumeric = (str) => {
+          let code, i, len;
+          for (i = 0, len = str.length; i < len; i++) {
+            code = str.charCodeAt(i);
+            if (!(code > 47 && code < 58) && !(code > 64 && code < 91) && !(code > 96 && code < 123) && code !== 32) {
+              return false;
+            }
+          }
+          return true;
+        };
 
-        if (!checkChars) return message.channel.send("Invalid character(s) provided.\nPlease ensure you have only provided alphanumeric characters.");
+        if (!isAlphaNumeric(text)) return message.channel.send("Invalid character(s) provided.\nPlease ensure you have only provided alphanumeric characters.");
 
         let output = "";
 
@@ -78,11 +87,14 @@ class BigLetter extends Command {
             value = ":nine: ";
             break;
         }
-
         return value;
       };
 
-      message.channel.send(generate());
+      message.channel.send(generate())
+        .catch(error => {
+          if (error.message === "Cannot send an empty message") return;
+          else this.client.logger.error(error.message);
+        });
     }
 }
 
