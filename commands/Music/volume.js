7b5bf3aff6f5fd4767752ccf682a98326c842f4b
Suvan <mr.slrmx@gmail.com>
2018-11-17 17:30:14 +0000

    Volume command

--- commands/Music/volume.js
+++ commands/Music/volume.js
@@ -12,8 +12,15 @@ class Volume extends Command {
       });
     }
 
-    async run(message, args, level) { // eslint-disable-line no-unused-vars
-        // See README.md to see why there is no code in here.
+    async run(message, args, level, settings, texts, serverQueue) { // eslint-disable-line no-unused-vars
+      if (!message.member.voiceChannel) return message.channel.send(texts.music.noVoiceChannel);
+      if (!serverQueue) return message.channel.send("There is nothing currently playing.");
+      if (!args[0]) return message.channel.send(`The current volume is **${serverQueue.volume}**.`);
+
+      serverQueue.volume = args[0];
+      serverQueue.connection.dispatcher.setVolumeLogarithmic((args[0].toLowerCase() === "default" ? 5 : args[0]) / 5);
+
+      return message.channel.send(`Volume set to **${args[0]}**.\nThe default volume level is 5.`);
     }
 }
 
