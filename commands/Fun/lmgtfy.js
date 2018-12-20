ac5736b9e442997da5ea99a6afa8e134811d33e8
Suvan <mr.slrmx@gmail.com>
2018-12-20 15:39:19 +0000

    Update LMGTFY command

--- commands/Fun/lmgtfy.js
+++ commands/Fun/lmgtfy.js
@@ -1,5 +1,4 @@
 const Command = require("../../base/Command.js");
-const encode = require("strict-uri-encode");
 
 class LMGTFY extends Command {
     constructor(client) {
@@ -14,7 +13,7 @@ class LMGTFY extends Command {
 
     async run(message, args, level, settings) { // eslint-disable-line no-unused-vars
         const textQuery = args.join(" ");
-        const query = encode(args.join(" "));
+        const query = encodeURIComponent(args.join(" "));
         const url = `https://lmgtfy.com/?q=${query}`;
 
         if (!query) return message.channel.send(`Please enter a query. For example, \`${settings.prefix}lmgtfy How to create a Discord server\`.`);
