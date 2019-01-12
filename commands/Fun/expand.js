b87f59f981b7421f59f0215ca5a8d61fda6f24ad
Suvan <mr.slrmx@gmail.com>
2019-01-12 12:07:11 +0000

    Update fun commands to node-fetch

--- commands/Fun/expand.js
+++ commands/Fun/expand.js
@@ -1,5 +1,5 @@
 const Command = require("../../base/Command.js");
-const snekfetch = require("snekfetch");
+const fetch = require("node-fetch");
 
 class Expand extends Command {
     constructor(client) {
@@ -14,14 +14,18 @@ class Expand extends Command {
     async run(message, args, level, settings, texts) { // eslint-disable-line no-unused-vars
         const text = encodeURIComponent(args.join(" "));
         if (!text) return message.channel.send("You must provide some text to expand.");
-        try {
-            const { body } = await snekfetch.get(`http://artii.herokuapp.com/make?text=${text}`);
-            if (body.length > 2000) return message.channel.send("Unfortunately, the specified text is too long. Please try again with something a little shorter.");
-            return message.channel.send(body, { code: "fix" });
-        } catch (error) {
-            this.client.logger.error(error);
-            return message.channel.send(texts.general.error.replace(/{{err}}/g, error));
-        }
+        const tooLong = "Unfortunately, the specified text is too long. Please try again with something a little shorter.";
+        
+        fetch(`http://artii.herokuapp.com/make?text=${text}`)
+            .then(res => res.text())
+            .then(body => {
+                if (body.length > 2000) return message.channel.send(tooLong);
+                return message.channel.send(body, { code: "fix" });
+            })
+            .catch(error => {
+                this.client.logger.error(error);
+                return message.channel.send(texts.general.error.replace(/{{err}}/g, error.message));
+            });
     }
 }
 
